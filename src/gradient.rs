dd30690afa3446b3e80ddcebb598cab8e4d1bb24
David Tolnay <dtolnay@gmail.com>
2020-04-26 14:42:54 -0700

    Clamp incoming gradient parameters

--- src/gradient.rs
+++ src/gradient.rs
@@ -1,4 +1,5 @@
 use crate::color::Color;
+use std::cmp;
 
 #[derive(Copy, Clone)]
 pub struct Gradient {
@@ -7,10 +8,18 @@ pub struct Gradient {
 
 impl Gradient {
     pub fn eval_rational(&self, i: usize, n: usize) -> Color {
+        let i = cmp::min(i, n - 1);
         self.eval.eval_rational(i, n)
     }
 
     pub fn eval_continuous(&self, t: f64) -> Color {
+        let t = if t < 0.0 {
+            0.0
+        } else if t > 1.0 {
+            1.0
+        } else {
+            t
+        };
         self.eval.eval_continuous(t)
     }
 }
