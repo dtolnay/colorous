632eb136ab20f4b260c40171255b8e47624ac722
David Tolnay <dtolnay@gmail.com>
2020-04-26 10:04:05 -0700

    Add sequential-single color schemes

---
+++ src/sequential_single.rs
@@ -0,0 +1,73 @@
+use crate::Color;
+
+pub const BLUES: &[Color; 9] = colors! {
+    b"f7fbff",
+    b"deebf7",
+    b"c6dbef",
+    b"9ecae1",
+    b"6baed6",
+    b"4292c6",
+    b"2171b5",
+    b"08519c",
+    b"08306b",
+};
+
+pub const GREENS: &[Color; 9] = colors! {
+    b"f7fcf5",
+    b"e5f5e0",
+    b"c7e9c0",
+    b"a1d99b",
+    b"74c476",
+    b"41ab5d",
+    b"238b45",
+    b"006d2c",
+    b"00441b",
+};
+
+pub const GREYS: &[Color; 9] = colors! {
+    b"ffffff",
+    b"f0f0f0",
+    b"d9d9d9",
+    b"bdbdbd",
+    b"969696",
+    b"737373",
+    b"525252",
+    b"252525",
+    b"000000",
+};
+
+pub const ORANGES: &[Color; 9] = colors! {
+    b"fff5eb",
+    b"fee6ce",
+    b"fdd0a2",
+    b"fdae6b",
+    b"fd8d3c",
+    b"f16913",
+    b"d94801",
+    b"a63603",
+    b"7f2704",
+};
+
+pub const PURPLES: &[Color; 9] = colors! {
+    b"fcfbfd",
+    b"efedf5",
+    b"dadaeb",
+    b"bcbddc",
+    b"9e9ac8",
+    b"807dba",
+    b"6a51a3",
+    b"54278f",
+    b"3f007d",
+};
+
+pub const REDS: &[Color; 9] = colors! {
+    b"fff5f0",
+    b"fee0d2",
+    b"fcbba1",
+    b"fc9272",
+    b"fb6a4a",
+    b"ef3b2c",
+    b"cb181d",
+    b"a50f15",
+    b"67000d",
+};
