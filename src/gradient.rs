c251281de9d61ba875d54112bdd54d523cca79e1
David Tolnay <dtolnay@gmail.com>
2020-04-26 14:37:19 -0700

    Add i/n rational evaluation for gradient

--- src/gradient.rs
+++ src/gradient.rs
@@ -6,11 +6,23 @@ pub struct Gradient {
 }
 
 impl Gradient {
+    pub fn eval_rational(&self, i: usize, n: usize) -> Color {
+        self.eval.eval_rational(i, n)
+    }
+
     pub fn eval_continuous(&self, t: f64) -> Color {
         self.eval.eval_continuous(t)
     }
 }
 
 pub(crate) trait EvalGradient {
+    fn eval_rational(&self, i: usize, n: usize) -> Color {
+        if n <= 1 {
+            self.eval_continuous(1.0)
+        } else {
+            self.eval_continuous(i as f64 / (n - 1) as f64)
+        }
+    }
+
     fn eval_continuous(&self, t: f64) -> Color;
 }
