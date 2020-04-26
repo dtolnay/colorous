355e4eaf53ded4f4ad344fd385df3359b0c43b7b
David Tolnay <dtolnay@gmail.com>
2020-04-26 09:12:29 -0700

    Add LowerHex and UpperHex formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
+use std::fmt::{self, LowerHex, UpperHex};
+
 #[derive(Copy, Clone, Debug)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
+}
+
+impl LowerHex for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02x}{:02x}{:02x}", self.r, self.g, self.b)
+    }
+}
+
+impl UpperHex for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:02X}{:02X}{:02X}", self.r, self.g, self.b)
+    }
 }
