2a3d03e9ca79dc9c04f0b4a636cd7c12d77c0887
David Tolnay <dtolnay@gmail.com>
2020-04-26 14:14:56 -0700

    Move Color struct to module

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,12 @@
 #[macro_use]
 mod macros;
 
+mod color;
+mod interpolate;
+
 pub mod categorical;
 pub mod diverging;
 pub mod sequential_multi;
 pub mod sequential_single;
 
-mod interpolate;
-
-use std::fmt::{self, Debug, LowerHex, UpperHex};
-
-#[derive(Copy, Clone)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-}
-
-impl Debug for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("Color(#")?;
-        LowerHex::fmt(self, f)?;
-        f.write_str(")")
-    }
-}
-
-impl LowerHex for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02x}{:02x}{:02x}", self.r, self.g, self.b)
-    }
-}
-
-impl UpperHex for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02X}{:02X}{:02X}", self.r, self.g, self.b)
-    }
-}
+pub use crate::color::Color;
