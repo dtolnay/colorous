545496192a1b853e7c17ee41012562e13fb3f6d1
David Tolnay <dtolnay@gmail.com>
2020-04-26 21:13:36 -0700

    Test that gradients have no discontinuities

--- tests/test_eval.rs
+++ tests/test_eval.rs
@@ -1,5 +1,5 @@
 #[test]
-fn test_eval() {
+fn test_continuous() {
     let gradients = vec![
         colorous::BLUES,
         colorous::BLUE_GREEN,
@@ -41,10 +41,23 @@ fn test_eval() {
         colorous::YELLOW_ORANGE_RED,
     ];
 
+    macro_rules! assert_continuous {
+        ($prev:ident, $value:expr) => {
+            let value: u8 = $value;
+            assert!(((value as i16) - ($prev as i16)).abs() <= 1);
+            $prev = value;
+        };
+    }
+
     let n = 10000;
     for g in &gradients {
-        for i in 0..n {
-            let _ = g.eval_rational(i, n);
+        let first = g.eval_rational(0, n);
+        let [mut prev_r, mut prev_g, mut prev_b] = first.as_array();
+        for i in 1..n {
+            let color = g.eval_rational(i, n);
+            assert_continuous!(prev_r, color.r);
+            assert_continuous!(prev_g, color.g);
+            assert_continuous!(prev_b, color.b);
         }
     }
 }
