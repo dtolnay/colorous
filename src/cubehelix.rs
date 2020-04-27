d467c71e0022dbcbe06f46c3c883186c20fc4492
David Tolnay <dtolnay@gmail.com>
2020-04-26 20:48:59 -0700

    Fold some negative signs in cubehelix conversion

--- src/cubehelix.rs
+++ src/cubehelix.rs
@@ -16,8 +16,8 @@ impl From<Cubehelix> for Color {
         let a = c.s * l * (1.0 - l);
         let cosh = h.cos();
         let sinh = h.sin();
-        let r = (255.0 * (l + a * (-0.14861 * cosh + 1.78277 * sinh)).min(1.0)) as u8;
-        let g = (255.0 * (l + a * (-0.29227 * cosh - 0.90649 * sinh)).min(1.0)) as u8;
+        let r = (255.0 * (l - a * (0.14861 * cosh - 1.78277 * sinh)).min(1.0)) as u8;
+        let g = (255.0 * (l - a * (0.29227 * cosh + 0.90649 * sinh)).min(1.0)) as u8;
         let b = (255.0 * (l + a * (1.97294 * cosh)).min(1.0)) as u8;
         Color { r, g, b }
     }
