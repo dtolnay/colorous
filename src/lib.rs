a4131708e2f05d2377964981896ff62dbc9b027b
David Tolnay <dtolnay@gmail.com>
2021-04-28 08:22:29 -0700

    Release 1.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -271,7 +271,7 @@
 //! Ten categorical colors authored by Tableau as part of [Tableau 10](https://www.tableau.com/about/blog/2016/7/colors-upgrade-tableau-10-56782).
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/colorous/1.0.4")]
+#![doc(html_root_url = "https://docs.rs/colorous/1.0.5")]
 #![warn(clippy::pedantic)]
 #![allow(
     clippy::cast_lossless,
